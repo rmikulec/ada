3fa9775747cf598c76b3f8cdf04792c68ae8b17b
Ryan Mikulec <ryan.mikulec@mikulec.net>
2023-12-23 00:50:25 -0500

    Bug fixes

--- backend/scixplain/app.py
+++ backend/scixplain/app.py
@@ -5,8 +5,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from scixplain.communicator import AsyncCommunicator as Communicator
-from scixplain.datasources.wiki import AsyncWikiSearch as WikiSearch
-from scixplain.datasources.web import AsyncWebSearch as WebSearch
+from scixplain.datasources.wiki import WikiSearch as WikiSearch
+from scixplain.datasources.general import AsyncWebSearch as WebSearch
 from scixplain.datasources.arxiv import ArxivSearch
 from scixplain.models import QuestionRequest, QuestionResponse, ResourceUsed, ResourceTypes
 
@@ -31,24 +31,11 @@ app.add_middleware(
 # Define the 'ask' route
 @app.post("/ask", response_model=QuestionResponse)
 async def ask(request: QuestionRequest) -> QuestionResponse:
-    datasources = [
-        WikiSearch(
-            question=request.question,
-            n_pages=request.config.n_pages,
-            n_sections=request.config.n_sections,
-        ),
-        WebSearch(question=request.question, n_articles=request.config.n_pages),
-        ArxivSearch(
-            question=request.question,
-            n_pages=request.config.n_pages,
-        ),
-    ]
-
     communicator = Communicator(
         initial_question=request.question,
         age=request.age,
         experience=request.experience,
-        datasources=datasources,
+        datasources=request.datasources,
     )
 
     await communicator.run()
@@ -61,32 +48,6 @@ async def ask(request: QuestionRequest) -> QuestionResponse:
         content = json.loads(communicator.messages[-1].content)
 
     resources = []
-
-    for datasource in datasources:
-        if type(datasource) == WikiSearch:
-            resources.extend(
-                [
-                    ResourceUsed(
-                        url=page.url,
-                        sections=page.sections,
-                        references=page.references,
-                        type=ResourceTypes.WIKIPEDIA,
-                    )
-                    for page in datasource.pages
-                ]
-            )
-        elif type(datasource) == ArxivSearch:
-            resources.extend(
-                [
-                    ResourceUsed(
-                        url=paper.title,
-                        sections=paper.categories,
-                        references=[link.href for link in paper.links],
-                        type=ResourceTypes.ARXIV,
-                    )
-                    for paper in datasource.papers
-                ]
-            )
 
     return QuestionResponse(
         markdown=content["markdown"], references=content["refs_used"], resources=resources
